3979710542dba26267a87f15b0604a18d41ab33c
aelgmal7 <aelgmal7@gmail.com>
2022-04-25 00:11:47 +0200

    adding dynamic list (get -delete)

--- src/app/components/tasks/task-item/task-item.component.ts
+++ src/app/components/tasks/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit ,Input} from '@angular/core';
+import { Component, OnInit ,Input,Output, EventEmitter } from '@angular/core';
 import { Task} from '../../../Task'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-item',
@@ -8,9 +10,15 @@ import { Task} from '../../../Task'
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!:Task;
+  @Output() onDeleteTask:EventEmitter<any> = new EventEmitter();
+  ico=faTimes
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onDelete(task: any){
+    this.onDeleteTask.emit(task);
   }
 
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Tasks} from '../../mock-tasks'
+import { TaskService } from 'src/app/service/task.service';
 import {Task} from '../../Task'
 
 @Component({
@@ -8,10 +8,20 @@ import {Task} from '../../Task'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks:Task[] = Tasks;
-  constructor() { }
+  tasks:Task[] = [];
+  constructor(private tasksService:TaskService) { }
 
   ngOnInit(): void {
+    this.tasksService.getTasks().subscribe((tasks:Task[]) => {
+      this.tasks=tasks;
+    })
+  }
+  deleteTask(task:Task){
+    this.tasksService.deleteTask(task).subscribe(()=>{
+      this.tasks= this.tasks.filter(t=> t.id !== task.id)
+      console.log(this.tasks)
+    })
+
   }
 
 }
